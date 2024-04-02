dcde96e227cc9400dc123180c1e76674f9027c27
Jacob Labelle <jacob.labelle93@gmail.com>
2024-04-01 20:34:18 -0700

    Create final-project-dns-poisoning.sh
    Init for final project ethical hacking activity

---
+++ final-project-dns-poisoning.sh
@@ -0,0 +1,11 @@
+# https://www.kali.org/docs/virtualization/install-vmware-silicon-host/
+# STEP 1: https://www.tutorialspoint.com/ethical_hacking/ethical_hacking_arp_poisoning.htm
+# STEP 2: https://www.tutorialspoint.com/ethical_hacking/ethical_hacking_dns_poisoning.htm
+
+# Requirements
+# Kali VM or VM with ettercap installed
+# Target VM - know the target's IP address and the gateway you need to spoof
+# Target IP address and redirect 
+# Network adapter must be in promiscuous mode
+
+# use nslookup or ping
